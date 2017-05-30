ca9bcfceb2d95a3257bdfdc447c67e5baaf11a59
Fatih DOĞAN <fatih.dogan@peakup.org>
2017-05-30 22:48:47 +0300

    Initial create.

---
+++ Sample.NPOI.Client/ItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.NPOI.Client
+{
+    public class ItemModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
--- Sample.NPOI.Client/MainWindow.xaml.cs
+++ Sample.NPOI.Client/MainWindow.xaml.cs
@@ -12,17 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
+using NPOI.Extension;
 
 namespace Sample.NPOI.Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dg.ItemsSource = new List<ItemModel>();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+                Excel.Load<ItemModel>(ofd.FileName);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if(sfd.ShowDialog() == true)
+                dg.ItemsSource.Cast<ItemModel>().ToExcel(sfd.FileName);
         }
     }
 }
